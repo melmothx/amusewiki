4cb89c15bb6a24e4a29855667ebc76eda171efee
Marco <marco@theanarchistlibrary.org>
2014-01-18 16:07:16 +0100

    Added view

---
+++ lib/AmuseWikiFarm/View/HTML.pm
@@ -0,0 +1,35 @@
+package AmuseWikiFarm::View::HTML;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+AmuseWikiFarm::View::HTML - TT View for AmuseWikiFarm
+
+=head1 DESCRIPTION
+
+TT View for AmuseWikiFarm.
+
+=head1 SEE ALSO
+
+L<AmuseWikiFarm>
+
+=head1 AUTHOR
+
+Marco,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/view_HTML.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'AmuseWikiFarm::View::HTML' }
+
+done_testing();
