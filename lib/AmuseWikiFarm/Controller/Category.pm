cd58d1c842b9f8deb54e1caf94c5e9d804d39d6b
Marco <marco@theanarchistlibrary.org>
2014-01-31 19:44:08 +0100

    Implemented listing

--- lib/AmuseWikiFarm/Controller/Category.pm
+++ lib/AmuseWikiFarm/Controller/Category.pm
@@ -52,7 +52,8 @@ sub authors :Global :Args(0) {
 
 sub author :Path('/authors') :Args(1) {
     my ( $self, $c, $arg ) = @_;
-    $c->res->body($c->uri_for($c->action, $arg));
+
+    $c->detach(category_details => [ author => $arg ]);
 }
 
 sub topics :Global :Args(0) {
@@ -65,11 +66,26 @@ sub topic :Path('/topics') :Args(1) {
     my ( $self, $c, $arg ) = @_;
 
     $c->detach(category_details => [ topic => $arg ]);
-    $c->res->body($c->uri_for($c->action));
 }
 
 sub category_details :Private {
     my ( $self, $c, $type, $name ) = @_;
+    my $locale = $c->stash->{locale};
+    my $id = $c->stash->{site_id};
+    $c->stash(baseurl => $c->uri_for_action('/library/index'));
+    my $cat = $c->model('DB::Category')->single({
+                                                 site_id => $id,
+                                                 type => $type,
+                                                 uri  => $name,
+                                                });
+    # not found unless $cat;
+    return unless $cat;
+    my $list = $c->model('DB::Category')->list_titles($cat->id, $locale);
+    $c->stash(
+              template => 'category-details.tt',
+              category => $cat,
+              list => $list
+             );
 }
 
 sub category_listing :Private {
@@ -77,14 +93,11 @@ sub category_listing :Private {
     my $loc = $c->stash->{locale};
     my $id = $c->stash->{site_id};
     my $list = $c->model('DB::Category')->listing($id, $type, $loc);
-
-    # manipulate the full uri
-    foreach my $el (@$list) {
-        $el->{uri} = $c->uri_for_action("/category/$type", $el->{uri});
-    }
-    # stash
-    $c->stash(list => $list);
-    $c->stash(template => 'category.tt');
+    $c->stash(
+              list => $list,
+              template => 'category.tt',
+              baseurl => $c->uri_for_action("/category/$type"),
+             );
 }
 
 
--- lib/AmuseWikiFarm/Schema/ResultSet/Category.pm
+++ lib/AmuseWikiFarm/Schema/ResultSet/Category.pm
@@ -16,17 +16,24 @@ uri, title count.
 
 sub listing {
     my ($self, $id, $type, $locale) = @_;
-    $id ||= 'default';
-    my @titles = $self->search({
-                                site_id => $id,
-                                type    => $type,
-                               });
+    my @cats = $self->search({
+                              site_id => $id,
+                              type    => $type,
+                             });
 
+    return $self->_sort_titles($locale, \@cats, "name");
+}
+
+sub list_titles {
+    my ($self, $id, $locale) = @_;
+    my @titles = $self->find($id)->titles;
+    return $self->_sort_titles($locale, \@titles, "title");
+}
+
+sub _sort_titles {
+    my ($self, $locale, $list, $method) = @_;
     my $collator = Unicode::Collate::Locale->new(locale => $locale);
-    my @result = map { { name => $_->name, uri => $_->uri, count => $_->title_count } }
-      @titles;
-
-    @titles = sort { $collator->cmp($a->{name}, $b->{name}) } @result;
+    my @titles = sort { $collator->cmp($a->$method, $b->$method) } @$list;
     return \@titles;
 }
 
