ace950f23b37e1cf7341ed38c3801972664e7977
Marco <marco@theanarchistlibrary.org>
2014-03-01 18:46:18 +0100

    Moved forward-compatible option in the package

--- lib/AmuseWikiFarm.pm
+++ lib/AmuseWikiFarm.pm
@@ -61,6 +61,7 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
+    encoding => 'UTF-8',
 );
 
 
