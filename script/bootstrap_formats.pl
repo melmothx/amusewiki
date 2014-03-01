76634c838d51e6a03d143f2eb91dd1e1c1a61355
Marco <marco@theanarchistlibrary.org>
2014-03-01 21:09:16 +0100

    Added script to compile all the muse files in a repo
    This is meant to be used to bootstrap a new repo with existing texts,
    or to rebuild them completely.

---
+++ script/bootstrap_formats.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use File::Spec::Functions qw/catdir catfile/;
+use File::Find;
+
+use Text::Amuse::Compile;
+
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+use Cwd;
+use AmuseWikiFarm::Model::DB;
+use Data::Dumper;
+
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDERR, ':encoding(UTF-8)';
+
+my $db = AmuseWikiFarm::Model::DB->new;
+
+my @todo;
+foreach my $s ($db->resultset('Site')->all) {
+    push @todo, $s;
+}
+
+$| = 1;
+
+die "Missing repo dir!" unless -d 'repo';
+chdir 'repo' or die $!;
+for my $site (@todo) {
+    print "Compiling all in " . $site->id . ":\n";
+    if (-d $site->id) {
+        print "Starting (" . localtime() . ")\n";
+        compile_all($site);
+        print "Done (" . localtime() . ")\n";
+        
+    }
+    else {
+        print "Skipping, " . $site->id . " not found\n";
+    }
+}
+
+sub compile_all {
+    my $site = shift;
+
+      
+    my $logfile = $site->id . ".error.log";
+    my $report = sub {
+        my @errors = @_;
+        open (my $fh, '>>:encoding(utf-8)', $logfile) or die $!;
+        print $fh @errors, "\n";
+        close $fh;
+    };
+    $report->("starting at " . localtime() . "\n");
+    my %opts = $site->compile_options;
+    my $c = Text::Amuse::Compile->new(
+                                      %opts,
+                                      report_failure_sub => $report,
+                                     );
+    $Data::Dumper::Deparse = 1;
+    print Dumper($c);
+    my @files;
+    find(sub {
+             my $file = $_;
+             return unless -f $file;
+             return unless index($File::Find::dir, '.git') < 0;
+             if ($file =~ m/\.muse$/) {
+                 push @files, $File::Find::name;
+             }
+         }, $site->id);
+    print Dumper(\@files);
+    $c->compile(@files);
+    $report->("Ended at " . localtime() . "\n");
+}
