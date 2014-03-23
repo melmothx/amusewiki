b090e2f1955c6af1671217f77cdf38435c8fb935
Marco <marco@theanarchistlibrary.org>
2014-03-23 19:38:58 +0100

    Improved status page

--- root/static/js/bookbuilder.js
+++ root/static/js/bookbuilder.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('.nojs').hide();
 });
 
+var BookBuilderStatus = '';
+
 function update_status(url) {
     $.getJSON(url, function(data) {
         console.log(data.status);
@@ -9,6 +11,24 @@ function update_status(url) {
             (data.status == 'taken')) {
             var funct = 'update_status("' + url + '")';
             setTimeout(funct, 1000);
+            console.log("Replacing with " + bbstatus[data.status]);
+        }
+        else if (data.status == 'completed') {
+            var phref = $('a.produced').attr('href');
+            $('a.produced').attr('href', phref + data.produced);
+            console.log(bbstatus[data.status]);
+        }
+        else if (data.status == 'failed') {
+            $('pre.errors').text(data.errors);
+            console.log(data.errors);
+        }
+
+        if (data.status != BookBuilderStatus) {
+            $('.bbstatus').hide();
+            $('span.bbstatusstring').text(bbstatus[data.status]);
+            var div = '.' + data.status;
+            $(div).show();
+            BookBuilderStatus = data.status;
         }
     });
 };
