163f95d3b3d7c703b6c192a241fe64de1658adce
Marco <marco@theanarchistlibrary.org>
2014-03-26 10:17:52 +0100

    Draft for /new
    CKeditor is configured now.

--- lib/AmuseWikiFarm/Controller/Edit.pm
+++ lib/AmuseWikiFarm/Controller/Edit.pm
@@ -1,4 +1,7 @@
 package AmuseWikiFarm::Controller::Edit;
+use strict;
+use warnings;
+use utf8;
 use Moose;
 use namespace::autoclean;
 
@@ -17,9 +20,53 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+=head2 /new
+
+Chained action for creation of new texts
+
+=head3 create
+
+Root chain element.
+
+=head3 render_created
+
+Path: /new
+
+The only purpose of this path is to present a form, which will post to
+
+
+=head3 add_created
+
+Path: /new/add
+
+
 
 =cut
+
+sub create :Chained('') :PathPart('new') :CaptureArgs(0) {
+    my ($self, $c) = @_;
+    $c->stash(template => 'edit/create.tt');
+    $c->stash(form_action => $c->uri_for_action('edit/add_created'));
+    my %available_languages = (
+                               ru => 'Русский',
+                               sr => 'Srpski',
+                               hr => 'Hrvatski',
+                               mk => 'Македонски',
+                               fi => 'Suomi',
+                               es => 'Español',
+                               en => 'English',
+                              );
+    $c->stash(known_langs => \%available_languages);
+}
+
+sub render_created :Chained('create') :PathPart('') :Args(0) {
+
+}
+
+sub add_created :Chained('create') :PathPart('add') :Args(0) {
+
+}
+
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
