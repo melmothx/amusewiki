ee69917269a5bba00be749d499e9b844994ac8db
Marco <marco@theanarchistlibrary.org>
2014-05-23 09:55:04 +0200

    CSS fixes

--- root/static/js/amuse.js
+++ root/static/js/amuse.js
@@ -7,4 +7,7 @@ $(document).ready(function(){
     $("div#thework table").addClass("table table-bordered");
     $(".center p").removeClass("text-justify").addClass("text-center");
     $(".right p").removeClass("text-justify").addClass("text-right");
+    $("div.table-of-contents").addClass("col-md-3");
+    $("div.table-of-contents").addClass("well");
+    $("div#thework").addClass("col-md-9");
 });
