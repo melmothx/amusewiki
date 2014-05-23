36bbba9ebd737e91bbd534029c9b3e8fa4e76db0
Marco <marco@theanarchistlibrary.org>
2014-05-23 17:28:05 +0200

    Adjust div width if the table of contents is present

--- root/static/js/amuse.js
+++ root/static/js/amuse.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
     $("div#thework table").addClass("table table-bordered");
     $(".center p").removeClass("text-justify").addClass("text-center");
     $(".right p").removeClass("text-justify").addClass("text-right");
-    $("div.table-of-contents").addClass("col-md-3");
-    $("div.table-of-contents").addClass("well");
-    $("div#thework").addClass("col-md-9");
+    if ($("div.table-of-contents").length > 0) {
+        $("div.table-of-contents").addClass("col-md-3");
+        $("div.table-of-contents").addClass("well");
+        $("div#thework").addClass("col-md-9");
+    }
+    else {
+        $("div#thework").addClass("col-md-12");
+    }
 });
