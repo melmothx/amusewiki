790f0b1f6c9fe630ddb5f6db078941edb7075794
Marco <marco@theanarchistlibrary.org>
2014-05-24 23:25:15 +0200

    Intercept term int and kill signals and exit when the job finished

--- script/jobber.pl
+++ script/jobber.pl
@@ -11,18 +11,35 @@ use Cwd;
 
 use AmuseWikiFarm::Schema;
 
+my $harakiri;
+
+sub do_harakiri {
+    $harakiri = 1;
+}
+
+$SIG{'TERM'} = 'do_harakiri';
+$SIG{'INT'}  = 'do_harakiri';
+$SIG{'KILL'} = 'do_harakiri';
+
+
 my $schema = AmuseWikiFarm::Schema->connect('amuse');
 my $queue = $schema->resultset('Job');
 my $cwd = getcwd();
 print "Starting job server loop in $cwd\n";
 
 while (1) {
+    if ($harakiri) {
+        print "Exiting as requested\n";
+        exit;
+    }
     chdir $cwd or die $!;
     sleep 3;
     my $job = $queue->dequeue;
     next unless $job;
+    print "Starting job on " . localtime() . "\n";
     print "Dispatching ", $job->id, " ", $job->status, " => ", $job->task, "\n";
     $job->dispatch_job;
+    print "Job finished on " . localtime() . "\n";
     chdir $cwd or die $!;
 }
 
