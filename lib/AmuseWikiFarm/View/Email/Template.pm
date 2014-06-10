5834454f6125c41934499cbf110e3b443d118a32
Marco <marco@theanarchistlibrary.org>
2014-06-10 09:21:21 +0200

    Dumped output of helper for Email::Template

---
+++ lib/AmuseWikiFarm/View/Email/Template.pm
@@ -0,0 +1,34 @@
+package AmuseWikiFarm::View::Email::Template;
+
+use strict;
+use base 'Catalyst::View::Email::Template';
+
+__PACKAGE__->config(
+    stash_key       => 'email',
+    template_prefix => ''
+);
+
+=head1 NAME
+
+AmuseWikiFarm::View::Email::Template - Templated Email View for AmuseWikiFarm
+
+=head1 DESCRIPTION
+
+View for sending template-generated email from AmuseWikiFarm.
+
+=head1 AUTHOR
+
+Marco,,,
+
+=head1 SEE ALSO
+
+L<AmuseWikiFarm>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/view_Email-Template.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'AmuseWikiFarm::View::Email::Template' }
+
+done_testing();
