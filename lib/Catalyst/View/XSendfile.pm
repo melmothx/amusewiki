878e1832088bfa9ae4c05610fcab0e86c5cb94d0
Marco <marco@theanarchistlibrary.org>
2014-06-28 19:32:02 +0200

    Set the mime type before setting the X-Accel thing

--- lib/Catalyst/View/XSendfile.pm
+++ lib/Catalyst/View/XSendfile.pm
@@ -3,7 +3,12 @@ use Moose;
 use namespace::autoclean;
  
 extends 'Catalyst::View';
- 
+
+# use Data::Dumper;
+use MIME::Types ();
+
+my $mime_types = MIME::Types->new(only_complete => 1);
+
 sub process {
     my ($self, $c) = @_;
     my $file = $c->stash->{serve_static_file};
@@ -11,15 +16,28 @@ sub process {
         $c->log->error("$file is not a file!");
         return;
     }
+    # $c->log->debug(Dumper($c->request->headers));
     $c->log->debug("Serving file $file");
     my ($header, $set) = $self->render($c, $file);
     if ($header && $set) {
         $c->response->header($header, $set);
+        my $type = $self->_ext_to_type($file);
+        $c->response->content_type($type);
         $c->response->body('');
+        $c->log->debug("Serving $header $set as $type");
     }
     else {
         $c->serve_static_file($file);
     }
+}
+
+sub _ext_to_type {
+    my ($self, $file) = @_;
+    my $type;
+    if ($file =~ m/\.(\w+)$/s) {
+        $type = $mime_types->mimeTypeOf($1);
+    }
+    $type ? return "$type" : return 'text/plain';
 }
 
 sub render {
