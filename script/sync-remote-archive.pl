d3f1de09f0ab5b14e2b9f40b08ae97b9670c4bc1
Marco <marco@theanarchistlibrary.org>
2014-10-22 11:17:26 +0200

    Added command line fetcher

---
+++ script/sync-remote-archive.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use lib 'lib';
+use AmuseWikiFarm::Schema;
+
+my ($site_id, $remote) = @ARGV;
+die "Missing site_id and/or remote" unless $site_id && $remote;
+my $schema = AmuseWikiFarm::Schema->connect('amuse');
+my $site = $schema->resultset('Site')->find($site_id);
+die "Site $site not found" unless $site;
+my $job = $site->jobs->git_action_add({ remote => $remote,
+                                          action => 'fetch' });
+my $id = $job->discard_changes->id;
+
+print "Job id is tasks/status/$id";
+
+# then loop and pool
+
+my $maxloop = 100;
+while ($job->status ne 'completed' and $job->status ne 'failed') {
+    $maxloop--;
+    if ($maxloop < 0) {
+        print "Waiting for job completion timed out\n";
+        last;
+    }
+    $job = $schema->resultset('Job')->find($id);
+    sleep 3;
+}
+
+print join("\n", map { $_ || '' }
+           $job->id , $job->site_id, $job->status,
+           $job->produced,
+           $job->errors, $job->logs);
