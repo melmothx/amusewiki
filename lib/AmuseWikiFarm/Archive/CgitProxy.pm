ec4444af882b8f7c92eafad260d0461e20fb63af
Marco <marco@theanarchistlibrary.org>
2015-07-23 10:59:49 +0200

    Create module for proxying cgit requests.

---
+++ lib/AmuseWikiFarm/Archive/CgitProxy.pm
@@ -0,0 +1,85 @@
+package AmuseWikiFarm::Archive::CgitProxy;
+
+use strict;
+use warnings;
+use URI;
+use HTTP::Tiny;
+use Encode qw//;
+use Moose;
+
+
+has port => (is => 'ro',
+             isa => 'Int',
+             default => '9015',
+            );
+
+has host => (is => 'ro',
+                 isa => 'Str',
+                 default => sub { 'localhost' });
+
+has scheme => (is => 'ro',
+               isa => 'Str',
+               default => sub { 'http' });
+
+has base_path => (is => 'ro',
+                  isa => 'Str',
+                  default => sub { 'git' });
+
+has ua => (is => 'ro',
+           isa => 'Object',
+           default => sub { HTTP::Tiny->new });
+
+has disabled => (is => 'ro',
+                 isa => 'Bool',
+                 lazy => 1,
+                 builder => '_build_disabled');
+
+sub _build_disabled {
+    my $self = shift;
+    my $test_uri = $self->create_uri;
+    my $res = $self->ua->get($test_uri);
+    return !$res->{success};
+}
+
+sub get_base_uri {
+    my $self = shift;
+    my $uri = $self->scheme . '://' . $self->host .
+      ($self->port ? ':' . $self->port : '');
+    return URI->new($uri);
+}
+
+sub create_uri {
+    my ($self, $args, $params) = @_;
+    $args ||= [];
+    $params ||= {};
+    unless (ref($args) and ref($args) eq 'ARRAY') {
+        die "first argument must be an arrayref";
+    }
+    unless (ref($params) and ref($params) eq 'HASH') {
+        die "second argument (params) must be a hashref";
+    }
+    my $uri = $self->get_base_uri;
+    my @path = ($self->base_path, @$args);
+    $uri->path(join('/', @path));
+    $uri->query_form($params) if %$params;
+    return $uri->as_string;
+}
+
+sub get_html {
+    my ($self, $args, $parms) = @_;
+    my $uri = $self->create_uri($args, $parms);
+    my $res = $self->ua->get($uri);
+    if ($res->{success} && $res->{content}) {
+        my $content;
+        eval { $content = Encode::decode('UTF-8', $res->{content}) };
+        $content ? return $content : return $res->{content};
+    }
+    else {
+        warn "$uri: $res->{status} $res->{reason}\n";
+        return '';
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
