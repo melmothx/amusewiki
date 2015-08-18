bd2acb13c13d0e14d4823b7ba365b85d3c35a8e7
Marco <marco@theanarchistlibrary.org>
2015-08-18 09:19:03 +0200

    Do not include the distro-wide fastcgi_params
    This file is distro-specific and got removed in jessie.
    Also, it makes sense to keep the params stable in a file we
    control.

--- script/generate-nginx-conf.pl
+++ script/generate-nginx-conf.pl
@@ -185,9 +185,28 @@ print $fh <<"INCLUDE";
         expires max;
     }
     location \@proxy {
-        include fastcgi_params;
-        fastcgi_param SCRIPT_NAME '';
-        fastcgi_param PATH_INFO   \$fastcgi_script_name;
+        fastcgi_param  QUERY_STRING       \$query_string;
+        fastcgi_param  REQUEST_METHOD     \$request_method;
+        fastcgi_param  CONTENT_TYPE       \$content_type;
+        fastcgi_param  CONTENT_LENGTH     \$content_length;
+
+        fastcgi_param  SCRIPT_NAME        '';
+        fastcgi_param  PATH_INFO          \$fastcgi_script_name;
+        fastcgi_param  REQUEST_URI        \$request_uri;
+        fastcgi_param  DOCUMENT_URI       \$document_uri;
+        fastcgi_param  DOCUMENT_ROOT      \$document_root;
+        fastcgi_param  SERVER_PROTOCOL    \$server_protocol;
+        fastcgi_param  HTTPS              \$https if_not_empty;
+
+        fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+        fastcgi_param  SERVER_SOFTWARE    nginx/\$nginx_version;
+
+        fastcgi_param  REMOTE_ADDR        \$remote_addr;
+        fastcgi_param  REMOTE_PORT        \$remote_port;
+        fastcgi_param  SERVER_ADDR        \$server_addr;
+        fastcgi_param  SERVER_PORT        \$server_port;
+        fastcgi_param  SERVER_NAME        \$server_name;
+
         fastcgi_param HTTP_X_SENDFILE_TYPE X-Accel-Redirect;
         fastcgi_param HTTP_X_ACCEL_MAPPING $amw_home=/private;
         fastcgi_pass  unix:$amw_home/var/amw.sock;
