ff785d225156cdf3cfafd03ea04f338ca2d5900d
Marco Pessotto <melmothx@gmail.com>
2015-10-26 11:33:30 +0100

    Configure Log4perl to read and watch log4perl.local.conf or log4perl.conf

--- lib/AmuseWikiFarm/Log/Contextual.pm
+++ lib/AmuseWikiFarm/Log/Contextual.pm
@@ -3,11 +3,24 @@ package AmuseWikiFarm::Log::Contextual;
 use strict;
 use warnings;
 use base 'Log::Contextual';
+use Log::Log4perl;
+use File::Spec;
+
+my $conf;
+if (-f 'log4perl.local.conf') {
+    $conf = 'log4perl.local.conf';
+}
+else {
+    $conf = 'log4perl.conf';
+}
+Log::Log4perl->init_and_watch($conf, 60);
  
-use Log::Log4perl ':easy';
-Log::Log4perl->easy_init($DEBUG);
- 
-sub arg_default_logger { $_[1] || Log::Log4perl->get_logger }
-sub default_import { qw(:dlog :log ) }
+sub arg_default_logger {
+    my ($self, $logger);
+    return $logger if $logger;
+    my $package = caller(3);
+    return Log::Log4perl->get_logger($package);
+}
+sub default_import { qw(:log ) }
 
 1;
