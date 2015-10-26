652457a5a73d8fe599de546d4c9fd85cbec22d34
Marco Pessotto <melmothx@gmail.com>
2015-10-26 14:22:01 +0100

    Do not init log4perl twice if already loaded

--- lib/AmuseWikiFarm/Log/Contextual.pm
+++ lib/AmuseWikiFarm/Log/Contextual.pm
@@ -8,15 +8,18 @@ use File::Spec;
 
 # note: if you cange engine, please change the engine in
 # AmuseWikiFarm.pm as well.
-my $conf;
-if (-f 'log4perl.local.conf') {
-    $conf = 'log4perl.local.conf';
+
+if (!Log::Log4perl->initialized) {
+    my $conf;
+    if (-f 'log4perl.local.conf') {
+        $conf = 'log4perl.local.conf';
+    }
+    else {
+        $conf = 'log4perl.conf';
+    }
+    Log::Log4perl->init_and_watch($conf, 60);
 }
-else {
-    $conf = 'log4perl.conf';
-}
-Log::Log4perl->init_and_watch($conf, 60);
- 
+
 sub arg_default_logger {
     my ($self, $logger);
     return $logger if $logger;
