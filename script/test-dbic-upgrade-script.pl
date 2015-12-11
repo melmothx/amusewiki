42cab3b88e6ab7102e8cb9771125ffdb3ef94587
Marco Pessotto <melmothx@gmail.com>
2015-12-11 16:08:51 +0100

    Added devel-only script

---
+++ script/test-dbic-upgrade-script.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+use AmuseWikiFarm::Schema;
+
+my $schema = AmuseWikiFarm::Schema->connect('amuse');
+
+my $file = $ARGV[0];
+die "first argument must be a file" unless $file && -f $file;
+
+my $sub = do "$file" or die $@;
+
+$sub->($schema);
+
+
