ae41d6525f1c25e048112f46b60f2311fad46a43
Marco Pessotto <melmothx@gmail.com>
2015-12-12 15:00:12 +0100

    Made install.pl non-interactive

--- script/install.pl
+++ script/install.pl
@@ -13,12 +13,27 @@ use AmuseWikiFarm::Schema;
 use Data::Dumper;
 use Try::Tiny;
 use Cwd;
+use Getopt::Long;
+use Crypt::XkcdPassword;
+
+my $hostname = `hostname -d` || 'localdomain';
+chomp $hostname;
+$hostname = 'amusewiki.' . $hostname;
+my $site_id = 'amw';
+my $password = Crypt::XkcdPassword->new(words => 'IT')->make_password(5, qr{\A[0-9a-zA-Z]{3,}\z});
+my $username = "amusewiki";
+
+GetOptions(
+           hostname => \$hostname,
+           username => \$username,
+           password => \$password,
+          ) or die;
 
 # load the db
 
 my $schema = AmuseWikiFarm::Schema->connect('amuse');
 
-die "Couldn't load the schema! Please check dbic.yaml!\n" unless $schema;
+die "Couldn't load the schema! Please check dbic.yaml!\n" unless $schema->storage->dbh;
 
 my $dh = DBIx::Class::DeploymentHandler->new({
                                               schema => $schema,
@@ -29,22 +44,8 @@ my $dh = DBIx::Class::DeploymentHandler->new({
                                                                      },
                                               script_directory => "$FindBin::Bin/../dbicdh",
                                              });
-# try if we have tables in this db. If so, bail out.
 
-print "BEWARE! I'm going to recreate the database from scratch!\n";
-print qq{Are you sure you want to continue? Please type "YES": };
-
-my $answer = ask_user();
-
-if ($answer eq 'YES') {
-    $dh->install;
-}
-else {
-    print "Ok, bailing out\n";
-    exit;
-}
-
-
+$dh->install;
 
 if (my @sites = $schema->resultset('Site')->all) {
     die "There are already sites in this database, aborting\n";
@@ -54,52 +55,18 @@ if (my @users = $schema->resultset('User')->all) {
     die "There are already user in this database, aborting\n";
 }
 
-
-# we need to collect some details
-print <<EOF;
-
-The database is ready.
-
-First we need to create a site. We need an host and a site code. The
-site code should be alphanumerical only, without any special
-character, should not start with a zero (reserved for testing) and
-have a maximum length of 16 characters, while the host should point to
-your server (without any protocol).
-
-EOF
-
-print qq{Please type the site's host (e.g. "example.org"): };
-my $host = ask_user();
-if ($host =~ m/^\s*([a-z0-9-]+(\.[a-z0-9-]+)*)\s*$/) {
-    $host = $1;
-    print qq{Host is "$host"\n};
+if ($hostname =~ m/^\s*([a-z0-9-]+(\.[a-z0-9-]+)*)\s*$/) {
+    $hostname = $1;
 }
 else {
-    die "Host name doesn't look valid\n";
+    die "Host name $hostname doesn't look valid\n";
 }
-
-print qq{Please type the site code (e.g. "mywiki"): };
-my $site_id = ask_user();
-if ($site_id =~ m/^\s*([1-9a-z][0-9a-z]{1,15})\s*$/) {
+if ($site_id =~ m/\A\s*([1-9a-z][0-9a-z]{1,15})\s*\z/) {
     $site_id = $1;
-    print qq{Site code is "$site_id"\n};
 }
 else {
     die "Site code contains illegal characters\n";
 }
-
-print <<EOF;
-
-Now we need to create a root user, which should be able to create
-librarians and new sites. Username should be alphanumerical only,
-lowercase, without any special character. For the initial password,
-please avoid non-ascii characters. Later you can change it on the web
-interface and set it to whatever you want.
-
-EOF
-
-print "Root account username: ";
-my $username = ask_user();
 
 if ($username =~ m/\s*([a-z0-9]+)\s*/) {
     $username = $1;
@@ -108,44 +75,40 @@ else {
     die qq{Bad username "$username"\n};
 }
 
-print "Root account password: ";
-my $password = ask_user();
-
-if ($password =~ m/^[[:ascii:]]+$/) {
-    
-}
-else {
+unless ($password =~ m/\A[[:ascii:]]+\z/) {
     die qq{Bad password, non ascii\n};
-}
-
-print <<"EOF";
-Summary of the initial setup:
-
-Host: $host
-Site ID: $site_id
-Root username: $username
-Root password: $password
-
-EOF
-
-print "Does it look good? (type YES if correct): ";
-my $confirm = ask_user();
-
-if ($confirm eq 'YES') {
-    print "Creating the site\n";
-}
-else {
-    die "Aborting\n";
 }
 
 my $site = $schema->resultset('Site')->create({
                                                id => $site_id,
-                                               canonical => $host,
+                                               canonical => $hostname,
+                                               sitename => "AmuseWiki documentation",
+                                               secure_site => 0, # otherwise first login will not work
+                                               cgit_integration => 1,
+                                               mode => 'private',
+                                               pdf => 0,
+                                               a4_pdf => 0,
+                                               lt_pdf => 0,
+                                               zip => 1,
+                                               epub => 1,
+                                               tex => 1,
                                               });
+$site->discard_changes;
+my $repo_root = $site->repo_root;
 
-if ($site->initialize_git) {
-    print "An stub repository has been created at " . $site->repo_root . "\n";
+unless (-d $repo_root) {
+    if (system(git => clone => "git://amusewiki.org/git/amw.git" => $site->repo_root) == 0) {
+        print "Repository with the official documentation has been created at " . $site->repo_root . "\n";
+    }
+    elsif ($site->initialize_git) {
+        print "An stub repository has been created at " . $site->repo_root . "\n";
+    }
+    else {
+        die "This shouldn't happen";
+    }
 }
+# compile
+$site->update_db_from_tree;
 
 my $user = $schema->resultset('User')->create({
                                                username => $username,
@@ -153,36 +116,11 @@ my $user = $schema->resultset('User')->create({
                                               });
 $user->set_roles([{ role => 'root' }]);
 
-print "\n";
-
-print "########## START NGINX configuration ##########\n\n";
-
 print <<"EOF";
+Summary of the initial setup:
 
-All done, please configure the webserver. If you use nginx, you can
-execute:
-
- ./script/generate-nginx-conf.pl
-
-which will generate a suitable configuration for nginx.
-
-After the webserver is configured, you can now start the app with
-
- ./init-all start.
-
-You may want to login at http://$host/login and then edit the site
-configuration at http://$host/admin/sites/edit/$site_id
-
-After the site configuration, you should bootstrap the repository with
-
- ./script/bootstrap_archive.pl
-
+Host: "$hostname"
+Site ID: "$site_id"
+Root username: "$username"
+Root password: "$password"
 EOF
-
-sub ask_user {
-    my $reply = <>;
-    chomp $reply;
-    return $reply;
-}
-
-
