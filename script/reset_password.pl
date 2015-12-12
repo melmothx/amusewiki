391011fa86b852c732e8fd2ef7eb29ffdcdcc062
Marco Pessotto <melmothx@gmail.com>
2015-12-12 15:50:34 +0100

    Use xkcd passwords in reset_passwords if password is not provided

--- script/reset_password.pl
+++ script/reset_password.pl
@@ -5,18 +5,19 @@ use strict;
 use warnings;
 use lib 'lib';
 use AmuseWikiFarm::Schema;
-
+use Crypt::XkcdPassword;
 
 my $schema = AmuseWikiFarm::Schema->connect('amuse');
 
 my ($username, $password) = @ARGV;
+$password ||= Crypt::XkcdPassword->new(words => 'IT')->make_password(5, qr{\A[0-9a-zA-Z]{3,}\z});
 
-die "Usage: $0 <username> <password>\n" unless $username && $password;
+die "Usage: $0 <username> [<password>]\n" unless $username && $password;
 
 if (my $user = $schema->resultset('User')->find({ username => $username })) {
     $user->password($password);
     $user->update;
-    print "Password for $username is now $password\n";
+    print qq{Password for $username is now '$password'\n};
 }
 else {
     die "No such user $username!";
