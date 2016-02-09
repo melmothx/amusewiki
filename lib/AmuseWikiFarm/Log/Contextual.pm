f6edafa295d38c149a77b6a991e2aaa431555f90
Marco Pessotto <melmothx@gmail.com>
2016-02-09 20:49:07 +0100

    Always use Log::Contextual
    The app uses L::C everywhere. The new wrapper module just intercepts the
    Catalyst logging.

--- lib/AmuseWikiFarm/Log/Contextual.pm
+++ lib/AmuseWikiFarm/Log/Contextual.pm
@@ -5,13 +5,27 @@ use warnings;
 use base 'Log::Contextual';
 use Log::Log4perl;
 use File::Spec;
-use AmuseWikiFarm::Utils::LogConf;
 
-# note: if you change engine, please change the engine in
-# AmuseWikiFarm.pm as well.
-
-if (!Log::Log4perl->initialized) {
-    Log::Log4perl->init(AmuseWikiFarm::Utils::LogConf::init_args);
+unless (Log::Log4perl->initialized) {
+    my $dummy =<<'CONF';
+log4perl.logger=INFO, Screen
+log4perl.appender.Screen=Log::Dispatch::Screen
+log4perl.appender.Screen.layout=Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern=%d %p - %c - %F{1}:%L - %m%n
+CONF
+    my $existing;
+    my @confs = (
+                 'log4perl.local.conf',
+                 'log4perl.conf',
+                );
+    foreach my $conf (@confs) {
+        my $path = File::Spec->rel2abs($conf);
+        if (-f $path) {
+            $existing = $path;
+            last;
+        }
+    }
+    Log::Log4perl->init($existing || \$dummy);
 }
 
 sub arg_default_logger {
---
+++ lib/AmuseWikiFarm/Log/Contextual/App.pm
@@ -0,0 +1,32 @@
+package AmuseWikiFarm::Log::Contextual::App;
+use 5.010001;
+use AmuseWikiFarm::Log::Contextual;
+
+# WARNING this module is dead-brained. Anyway, it appears to do the
+# job correctly and it just intercepts the Catalyst core logging.
+
+sub new {
+    my ($class, @opts) = @_;
+    my $self = {};
+    Dlog_debug { "Passed $_ to constructor" } \@opts;
+    bless $self, $class;
+}
+
+sub trace { my ($self, @stuff) = @_; log_trace { join("\n", @stuff) }; }
+sub debug { my ($self, @stuff) = @_; log_debug { join("\n", @stuff) }; }
+sub info  { my ($self, @stuff) = @_; log_info  { join("\n", @stuff) }; }
+sub warn  { my ($self, @stuff) = @_; log_warn  { join("\n", @stuff) }; }
+sub error { my ($self, @stuff) = @_; log_error { join("\n", @stuff) }; }
+sub fatal { my ($self, @stuff) = @_; log_fatal { join("\n", @stuff) }; }
+sub abort   { 0 };
+sub levels  { 0 };
+sub enable  { 0 };
+sub disable { 0 };
+sub is_trace { 1 };
+sub is_debug { 1 };
+sub is_info  { 1 };
+sub is_warn  { 1 };
+sub is_error { 1 };
+sub is_fatal { 1 };
+
+1;
