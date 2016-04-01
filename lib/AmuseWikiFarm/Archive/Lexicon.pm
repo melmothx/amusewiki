61f400a7024fe246d3a27bc711f222a2b6f2a7b8
Marco Pessotto <melmothx@gmail.com>
2016-04-01 10:41:57 +0200

    Implemented the freshness checking for local po

--- lib/AmuseWikiFarm/Archive/Lexicon.pm
+++ lib/AmuseWikiFarm/Archive/Lexicon.pm
@@ -19,12 +19,25 @@ use strict;
 use warnings;
 
 use Moo;
-use Types::Standard qw/Maybe Object/;
+use Types::Standard qw/Maybe Object Int Str InstanceOf/;
 use Try::Tiny;
+use Path::Tiny;
 use HTML::Entities qw/encode_entities decode_entities/;
 
 has global => (is => 'ro', isa => Object);
 has site => (is => 'ro', isa => Maybe[Object]);
+has local_file => (is => 'ro', isa => InstanceOf[qw/Path::Tiny/], required => 1);
+has local_file_timestamp => (is => 'ro', isa => Int, default => 0);
+
+sub is_obsolete {
+    my $self = shift;
+    if (my $po = $self->local_file) {
+        if (-f $po and $po->stat->mtime > $self->local_file_timestamp) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 sub loc {
     my ($self, $key, @args) = @_;
@@ -60,7 +73,7 @@ sub loc_html {
 package AmuseWikiFarm::Archive::Lexicon;
 
 use Moo;
-use Types::Standard qw/Str HashRef Object/;
+use Types::Standard qw/Str HashRef Object Int/;
 use Path::Tiny;
 use AmuseWikiFarm::Log::Contextual;
 
@@ -73,15 +86,23 @@ sub localizer {
     my ($self, $lang, $repo) = @_;
     $repo ||= 'amw';
     $lang ||= 'en';
+    die "Bad repo name" unless $repo =~ m/\A[a-z0-9]+\z/;
+    die "Bad lang name" unless $lang =~ m/\A[a-z0-9]+\z/;
     if (my $handler = $self->loaded->{$repo}->{$lang}) {
         log_debug { "$lang for $repo has already been loaded" };
-        return $handler;
+        if ($handler->is_obsolete) {
+            log_debug { "However, $lang for $repo is obsolete" };
+        }
+        else {
+            return $handler;
+        }
     }
     my $global = $self->globals->{$lang};
     unless ($global) {
         my $po = path($self->system_wide_po_dir, "$lang.po")->stringify;
         if (-f $po) {
             log_debug { "Loading $po file for $lang" };
+            # these are actual languages
             AmuseWikiFarm::Archive::Lexicon::Handles->import_po_file($lang, $po, 1);
         }
         else {
@@ -91,18 +112,27 @@ sub localizer {
         $self->globals->{$lang} = $global;
     }
     my $site;
-    my $local = path($self->repo_dir, $repo, locales => "$lang.po")->stringify;
+    my $local = path($self->repo_dir, $repo, locales => "$lang.po");
+    my $local_ts = 0;
     if (-f $local) {
-        my $internal = "i-amw-$repo-$lang";
-        log_debug { "Loading $local po" };
-        AmuseWikiFarm::Archive::Lexicon::Handles->import_po_file($internal, $local, 0);
-        $site = AmuseWikiFarm::Archive::Lexicon::Handles->get_handle($internal);
+        my $internal = "i_amw_${repo}_${lang}";
+        log_debug { "Loading $local with name $internal" };
+        # not actually documented in the pod, but in the source it is.
+        local $Locale::Maketext::USING_LANGUAGE_TAGS = 0;
+        AmuseWikiFarm::Archive::Lexicon::Handles->import_po_file($internal, "$local", 0);
+        $site = AmuseWikiFarm::Archive::Lexicon::Handles->get_handle($internal)
+          or die "$internal couldn't be loaded even if we imported it!";
+        Dlog_debug{ $_ } $site;
+        $local_ts = $local->stat->mtime;
     }
     else {
         log_debug { "$local doesn't exist, not loading" };
     }
     my $localizer = AmuseWikiFarm::Archive::Lexicon::Site->new(site => $site,
-                                                               global => $global);
+                                                               global => $global,
+                                                               local_file_timestamp => $local_ts,
+                                                               local_file => $local,
+                                                              );
     $self->loaded->{$repo}->{$lang} = $localizer;
     return $localizer;
 }
--- t/lexicon.t
+++ t/lexicon.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 use utf8;
-use Test::More tests => 11;
+use Test::More tests => 16;
 BEGIN { $ENV{DBIX_CONFIG_DIR} = "t" };
 
 my $builder = Test::More->builder;
@@ -51,12 +51,6 @@ write_file($site->lexicon_file, to_json({
                                          'test [_1] [_2] [_3]' => { it => '[_1] [_2] [_3] prova " ć100' },
                                         }));
 
-is ($site->lexicon_translate(it => 'test'), 'Prova');
-is ($site->lexicon_translate(it => '<test>'), '<Test>');
-is ($site->lexicon_translate(it => '&lt;test&gt;'), undef);
-is ($site->lexicon_translate(it => 'test [_1] [_2] [_3]', qw/uno due tre/),
-    "uno due tre prova \" ć100");
-
 my $temp = Path::Tiny->tempdir;
 foreach my $po (AmuseWikiFarm::Utils::LexiconMigration::convert($site->lexicon, $locales_dir)) {
     diag "$po";
@@ -65,13 +59,37 @@ foreach my $po (AmuseWikiFarm::Utils::LexiconMigration::convert($site->lexicon, 
     like $po_body, qr{%1 %2 %3 prova \\" ć100};
 }
 
-my $lh = AmuseWikiFarm::Archive::Lexicon->new(system_wide_po_dir => path(qw/lib AmuseWikiFarm I18N/)
-                                              ->absolute->stringify,
-                                              repo_dir => path("repo")
-                                              ->absolute->stringify)->localizer(it => $site->id);
-is ($lh->loc('test'), 'Prova') or diag Dumper($lh);
-is ($lh->loc('<test>'), '<Test>');
-is ($lh->loc('&lt;test&gt;'), '<Test>');
-is ($lh->loc('test [_1] [_2] [_3]', qw/uno due tre/), "uno due tre prova \" ć100");
-is ($lh->loc('test [_1] [_2] [_3]', [qw/uno due tre/]), "uno due tre prova \" ć100");
+my $model = AmuseWikiFarm::Archive::Lexicon->new(system_wide_po_dir => path(qw/lib AmuseWikiFarm I18N/)
+                                                 ->absolute->stringify,
+                                                 repo_dir => path("repo")
+                                                 ->absolute->stringify);
+{
+    my $lh = $model->localizer(it => $site->id);
+    is ($lh->loc('test'), 'Prova') or diag Dumper($lh);
+    is ($lh->loc('<test>'), '<Test>');
+    is ($lh->loc('&lt;test&gt;'), '<Test>');
+    is ($lh->loc('test [_1] [_2] [_3]', qw/uno due tre/), "uno due tre prova \" ć100");
+    is ($lh->loc('test [_1] [_2] [_3]', [qw/uno due tre/]), "uno due tre prova \" ć100");
+}
+
+{
+    my $lh = $model->localizer(hr => $site->id);
+    ok (! -f $lh->local_file, $lh->local_file . " doesn't exist");
+    is ($lh->loc('test'), 'test');
+    $lh->local_file->spew_utf8(qq{msgid "test"\nmsgstr "Proba"\n});
+    is ($lh->loc('test'), 'test');
+    ok($lh->is_obsolete);
+    # reload
+    $lh = $model->localizer(hr => $site->id);
+    diag $lh->local_file;
+    is ($lh->loc('test'), 'Proba');
+    sleep 1;
+    $lh->local_file->spew_utf8(qq{msgid ""\nmsgstr "Content-Type: "text/plain; charset=UTF-8\\n"\n\nmsgid "test"\nmsgstr "ćxProbaX"\n});
+    ok($lh->is_obsolete);
+    is ($lh->loc('test'), 'Proba');
+    # reload again
+    $lh = $model->localizer(hr => $site->id);
+    ok(!$lh->is_obsolete);
+    is ($lh->loc('test'), 'ćxProbaX');
+}
 
