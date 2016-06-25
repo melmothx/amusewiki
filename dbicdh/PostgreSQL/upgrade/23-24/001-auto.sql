052ddac883ba7f18126208d69dcd58d17eadcca4
Marco Pessotto <melmothx@gmail.com>
2016-06-25 11:25:57 +0200

    Empty db upgrade to call ->populate_monthly_archives on sites

---
+++ dbicdh/PostgreSQL/upgrade/23-24/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/23/001-auto.yml' to '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/24/001-auto.yml':;
+
+;
+-- No differences found;
+
--- lib/AmuseWikiFarm/Schema.pm
+++ lib/AmuseWikiFarm/Schema.pm
@@ -40,7 +40,7 @@ and provide a dbic.yaml file with something like this:
 
 =cut
 
-our $VERSION = 23;
+our $VERSION = 24;
 
 __PACKAGE__->load_components('Schema::Config');
 
