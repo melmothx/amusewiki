02d158bb0d473a8976325906ba2fb32ccfd07530
Marco Pessotto <melmothx@gmail.com>
2016-11-14 18:44:40 +0100

    Add "started" column to bulk_job

---
+++ dbicdh/SQLite/upgrade/29-30/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/29/001-auto.yml' to '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/30/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE bulk_job ADD COLUMN started datetime;
+
+;
+
+COMMIT;
+
