e76c1a2a236aaffc1113a23a3924e4404f279dce
Marco Pessotto <melmothx@gmail.com>
2016-11-14 13:32:20 +0100

    Update manifest

---
+++ lib/AmuseWikiFarm/Schema/ResultSet/BulkJob.pm
@@ -0,0 +1,31 @@
+package AmuseWikiFarm::Schema::ResultSet::BulkJob;
+
+use utf8;
+use strict;
+use warnings;
+use base 'DBIx::Class::ResultSet';
+use AmuseWikiFarm::Log::Contextual;
+
+sub active_bulk_jobs {
+    my $self = shift;
+    my $me = $self->current_source_alias;
+    return $self->search({ "$me.completed" => undef },
+                         { order_by => { -desc => "$me.created" } });
+}
+
+sub completed_jobs {
+    my $self = shift;
+    my $me = $self->current_source_alias;
+    return $self->search({ "$me.status" => 'completed' },
+                         { order_by => { -desc => "$me.completed" } });
+}
+
+sub abort_all {
+    my $self = shift;
+    while (my $job = $self->next) {
+        log_info { "Aborting bulk jobs for " . $job->site->canonical };
+        $job->abort_jobs;
+    }
+}
+
+1;
