199b05b2e7c9d70787644d274ee6cc354d1e1a65
Marco Pessotto <melmothx@gmail.com>
2017-05-14 11:51:45 +0200

    Create widget for sorting texts under categories

--- lib/AmuseWikiFarm/Controller/Category.pm
+++ lib/AmuseWikiFarm/Controller/Category.pm
@@ -187,12 +187,16 @@ sub single_category :Chained('category') :PathPart('') :CaptureArgs(1) {
 sub _stash_pager {
     my ($self, $c, $action, @args) = @_;
     my $pager = $c->stash->{texts}->pager;
+    my $site = $c->stash->{site};
     my ($rows, $sort);
+    my $active_rows = $pager->entries_per_page;
+    my $active_sort = $site->validate_text_category_sorting($c->request->query_params->{sort} ||
+                                                            $site->titles_category_default_sorting);
     if ($c->request->query_params->{rows}) {
-        $rows = $pager->entries_per_page;
+        $rows = $active_rows;
     }
     if (my $qsort = $c->request->query_params->{sort}) {
-        $sort = $c->stash->{site}->validate_text_category_sorting($qsort);
+        $sort = $active_sort;
     }
     my $format_link = sub {
         return $c->uri_for_action($action,
@@ -202,7 +206,28 @@ sub _stash_pager {
                                            ($sort ? (sort => $sort) : ()),
                                           });
     };
-    $c->stash(pager => AmuseWikiFarm::Utils::Paginator::create_pager($pager, $format_link));
+    my (@sortings, @rows_per_page);
+    foreach my $s ($site->titles_available_sortings) {
+        if ($s->{name} eq $active_sort) {
+            $s->{active} = 1;
+        }
+        push @sortings, $s;
+    }
+    foreach my $i (10, 20, 50, 100, 200, 500) {
+        push @rows_per_page, {
+                              rows => $i,
+                              active => ($i == $active_rows ? 1 : 0),
+                             };
+    }
+    Dlog_debug { "widget : $_ " } \@sortings;
+    $c->stash(
+              pager => AmuseWikiFarm::Utils::Paginator::create_pager($pager, $format_link),
+              pagination_widget => {
+                                    target => $c->uri_for_action($action, \@args),
+                                    sortings => \@sortings,
+                                    rows => \@rows_per_page,
+                                   },
+             );
 }
 
 sub single_category_display :Chained('single_category') :PathPart('') :Args(0) {
