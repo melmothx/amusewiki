fccc7b0e4e7c60e16693bc9f88688e905c3d9746
Marco Pessotto <melmothx@gmail.com>
2017-05-23 10:50:53 +0200

    Regular listing: include deferred with teaser if option  is on
    Deferred without teaser are for logged in only

--- lib/AmuseWikiFarm/Role/Controller/RegularListing.pm
+++ lib/AmuseWikiFarm/Role/Controller/RegularListing.pm
@@ -14,12 +14,16 @@ sub base :Chained('pre_base') :PathPart('') :CaptureArgs(0) {
     log_debug { 'in regular listing base' };
     # if the user is logged in, give him access to deferred as well
     my $titles = $c->stash->{site}->titles;
-    my $rs;
-    if ($c->user_exists || $c->stash->{site}->show_preview_when_deferred) {
-        $rs = $titles->published_or_deferred_texts;
+    my $rs = $titles->texts_only;
+    if ($c->user_exists) {
+        $rs = $rs->status_is_published_or_deferred;
+    }
+    elsif ($c->stash->{site}->show_preview_when_deferred) {
+        log_warn { "Asking for deferred with teaser" };
+        $rs = $rs->status_is_published_or_deferred_with_teaser;
     }
     else {
-        $rs = $titles->published_texts;
+        $rs = $rs->status_is_published;
     }
     $c->stash(
               f_class => 'text',
