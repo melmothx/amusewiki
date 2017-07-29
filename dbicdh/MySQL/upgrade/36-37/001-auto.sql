9ce5d34bf71855ddcebd555fbae6d32f1a18c481
Marco Pessotto <melmothx@gmail.com>
2017-07-29 17:16:42 +0200

    Generated DDLs (upgrade is empty)

---
+++ dbicdh/MySQL/upgrade/36-37/001-auto.sql
@@ -0,0 +1,5 @@
+-- Convert schema '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/36/001-auto.yml' to '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/37/001-auto.yml':;
+
+;
+-- No differences found;
+
