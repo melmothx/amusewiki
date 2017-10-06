ef72478e386dfbaf7376efaa0875a1af4de79059
Marco Pessotto <melmothx@gmail.com>
2017-10-06 08:37:20 +0200

    Add preferred language to users table

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -148,6 +148,7 @@ CREATE TABLE users (
        edit_option_show_filters INTEGER(1) NOT NULL DEFAULT 1,
        edit_option_show_cheatsheet INTEGER(1) NOT NULL DEFAULT 1,
        edit_option_page_left_bs_columns INTEGER DEFAULT 6,
+       preferred_language VARCHAR(8),
        reset_token TEXT NULL,
        reset_until INTEGER NULL
 );
