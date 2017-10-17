3cbcfcf68608eccd0448b5efd8231f53b2ce7516
Marco Pessotto <melmothx@gmail.com>
2017-10-17 11:32:52 +0200

    Remove old generated DDLs pre-dbicdh
    It is exactly two years amw switched to DBICDH and there is no reason
    to keep these files around.

--- sql/AmuseWikiFarm-Schema-0.91-0.95-PostgreSQL.sql
+++
@@ -1,9 +0,0 @@
--- Convert schema 'sql/AmuseWikiFarm-Schema-0.91-PostgreSQL.sql' to 'sql/AmuseWikiFarm-Schema-0.95-PostgreSQL.sql':;
-
-BEGIN;
-
-ALTER TABLE site ADD COLUMN nocoverpage integer DEFAULT 0 NOT NULL;
-
-
-COMMIT;
-
--- sql/AmuseWikiFarm-Schema-0.91-0.95-SQLite.sql
+++
@@ -1,9 +0,0 @@
--- Convert schema 'sql/AmuseWikiFarm-Schema-0.91-SQLite.sql' to 'sql/AmuseWikiFarm-Schema-0.95-SQLite.sql':;
-
-BEGIN;
-
-ALTER TABLE site ADD COLUMN nocoverpage integer(1) NOT NULL DEFAULT 0;
-
-
-COMMIT;
-
