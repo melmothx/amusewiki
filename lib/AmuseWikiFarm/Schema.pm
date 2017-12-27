30e5dbc16f05ee62e5f4dd9cedde9549572c543a
Marco Pessotto <melmothx@gmail.com>
2017-12-27 09:31:37 +0100

    Bump DB versions

--- lib/AmuseWikiFarm/Schema.pm
+++ lib/AmuseWikiFarm/Schema.pm
@@ -40,7 +40,7 @@ and provide a dbic.yaml file with something like this:
 
 =cut
 
-our $VERSION = 42;
+our $VERSION = 43;
 
 __PACKAGE__->load_components('Schema::Config');
 __PACKAGE__->load_components('Helper::Schema::QuoteNames');
