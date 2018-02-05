1f9497c3c5aec2da16b8d5cf0fe9be245f9a2522
Marco Pessotto <melmothx@gmail.com>
2018-02-05 16:07:00 +0100

    Improve the session dumper to pick to obey the configuration

--- script/dump_sessions.pl
+++ script/dump_sessions.pl
@@ -2,14 +2,26 @@
 
 use strict;
 use warnings;
-use File::Spec;
-use Data::Dumper;
+use lib 'lib';
+use AmuseWikiFarm;
+use Data::Dumper::Concise;
 use Cache::FastMmap;
+use File::Copy;
 
-my $f = File::Spec->rel2abs(File::Spec->catfile(qw/opt cache fastmmap/));
-my $c = Cache::FastMmap->new(raw_values => 0,
-                             unlink_on_exit => 0,
-                             init_file => 0,
-                             cache_size => '100m',
-                             share_file => File::Spec->rel2abs(File::Spec->catfile(qw/opt cache fastmmap/)));
-print Dumper([sort $c->get_keys(0)]);
+my $config = AmuseWikiFarm->_session_plugin_config;
+die Dumper($config) unless $config->{cache_size} && $config->{storage};
+copy($config->{storage}, $config->{storage} . '~' . time())
+  or die "$config->{storage}: Couldn't make a backup $!";
+
+if (-f $config->{storage}) {
+    my $c = Cache::FastMmap->new(raw_values => 0,
+                                 unlink_on_exit => 0,
+                                 init_file => 0,
+                                 cache_size => $config->{cache_size},
+                                 share_file => $config->{storage},
+                                );
+    print Dumper([sort $c->get_keys(2)]);
+}
+else {
+    print "$config->{storage} file doesn't exist, nothing to do\n";
+}
