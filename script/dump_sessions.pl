1c639db4325c5071edf5ca7be9d6209ef3d5bc91
Marco Pessotto <melmothx@gmail.com>
2018-02-06 15:39:22 +0100

    Prepare migration script for sessions

--- script/dump_sessions.pl
+++ script/dump_sessions.pl
@@ -4,24 +4,62 @@ use strict;
 use warnings;
 use lib 'lib';
 use AmuseWikiFarm;
+use AmuseWikiFarm::Schema;
 use Data::Dumper::Concise;
 use Cache::FastMmap;
-use File::Copy;
+use File::Copy qw/copy move/;
 
+my $schema = AmuseWikiFarm::Schema->connect('amuse');
+my %sessions;
 my $config = AmuseWikiFarm->_session_plugin_config;
-die Dumper($config) unless $config->{cache_size} && $config->{storage};
+unless ($config->{cache_size} && $config->{storage} && -f $config->{storage}) {
+    print "No need to migrate, exiting\n";
+    exit;
+}
 copy($config->{storage}, $config->{storage} . '~' . time())
   or die "$config->{storage}: Couldn't make a backup $!";
 
-if (-f $config->{storage}) {
-    my $c = Cache::FastMmap->new(raw_values => 0,
-                                 unlink_on_exit => 0,
-                                 init_file => 0,
-                                 cache_size => $config->{cache_size},
-                                 share_file => $config->{storage},
-                                );
-    print Dumper([sort $c->get_keys(2)]);
+my $c = Cache::FastMmap->new(raw_values => 0,
+                             unlink_on_exit => 0,
+                             init_file => 0,
+                             cache_size => $config->{cache_size},
+                             share_file => $config->{storage},
+                            );
+my @all = $c->get_keys(0);
+print "Retrieving " . scalar(@all) . " keys\n";
+while (@all) {
+    my $sid = shift @all;
+    my ($id, $field) = AmuseWikiFarm::Schema::ResultSet::AmwSession::_split_id_and_field($sid);
+    $field =~ s/_data$//;
+    my $value = $c->get($sid);
+    $sessions{$id}{$field} = $value;
+    if ($field eq 'session') {
+        $sessions{$id}{site_id} = $value->{site_id};
+    }
 }
-else {
-    print "$config->{storage} file doesn't exist, nothing to do\n";
+
+# print Dumper(\%sessions);
+
+my %sites = map { $_->id => 1 } $schema->resultset('Site')->all;
+
+my $now = time();
+my $guard = $schema->txn_scope_guard;
+my $importer = $schema->resultset('AmwSession');
+foreach my $sid (keys %sessions) {
+    if (my $site_id = $sessions{$sid}{site_id}) {
+        if ($sites{$site_id}) {
+            if ($sessions{$sid}{expires} and $sessions{$sid}{expires} > $now) {
+                foreach my $f (qw/expires session flash/) {
+                    if (exists $sessions{$sid}{$f}) {
+                        $importer->store_session_data($site_id, "$f:$sid", $sessions{$sid}{$f});
+                    }
+                }
+            }
+        }
+    }
 }
+
+move($config->{storage}, $config->{storage} . '~' . time())
+  or die "$config->{storage}: Couldn't move it $!";
+
+$guard->commit;
