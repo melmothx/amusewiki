78dccdfed7ab8b86684002eecd09a65a26dbefe9
Marco Pessotto <melmothx@gmail.com>
2018-06-16 13:53:44 +0200

    Add search-based RSS feeds

--- lib/AmuseWikiMeta/Controller/Search.pm
+++ lib/AmuseWikiMeta/Controller/Search.pm
@@ -7,7 +7,9 @@ BEGIN { extends 'Catalyst::Controller'; }
 use AmuseWikiFarm::Log::Contextual;
 use AmuseWikiFarm::Archive::Xapian;
 use AmuseWikiFarm::Utils::Paginator;
+use AmuseWikiFarm::Utils::Amuse qw/clean_html/;
 use Path::Tiny;
+use XML::FeedPP;
 
 sub search :Chained('/root') :CaptureArgs(0) {
     my ($self, $c) = @_;
@@ -19,7 +21,6 @@ sub ajax :Chained('search') :Args(0) {
     my $conf = $c->model('DB');
     my $stub = $conf->stub_database;
     log_debug { "Using $stub db" };
-    die "Cannot proceed without $stub stub database" unless -f $stub;
     my $xapian = AmuseWikiFarm::Archive::Xapian->new(multisite => 1,
                                                      stub_database => "$stub",
                                                     );
@@ -73,6 +74,58 @@ sub ajax :Chained('search') :Args(0) {
                       });
     # Dlog_debug { "json is $_" } $c->stash->{json};
 }
+
+sub feed :Chained('/root') :Args(0) {
+    my ($self, $c) = @_;
+    my $conf = $c->model('DB');
+    my $xapian = AmuseWikiFarm::Archive::Xapian->new(multisite => 1,
+                                                     stub_database => $conf->stub_database,
+                                                    );
+    my %params = %{$c->req->params};
+    # enforce sorting to be time-based.
+    $params{sort} = 'pubdate_desc';
+    my $res = $xapian->faceted_search(%params,
+                                      published_only => 1,
+                                      facets => 0,
+                                      filters => 1,
+                                     );
+    $res->sites_map($conf->site_map);
+    my @texts = @{$res->json_output};
+    Dlog_debug  { "Matches are $_ " } \@texts;
+    my $feed = XML::FeedPP::RSS->new;
+    my $my_uri = $c->req->uri;
+    $feed->title($my_uri->host);
+    $feed->description($my_uri->host);
+    $feed->link($c->uri_for_action($c->action));
+    $feed->language('en');
+    $feed->xmlns('xmlns:atom' => "http://www.w3.org/2005/Atom");
+    $feed->set('atom:link@href', $c->uri_for_action($c->action));
+    $feed->set('atom:link@rel', 'self');
+    $feed->set('atom:link@type', "application/rss+xml");
+    if (@texts) {
+        $feed->pubDate($texts[0]{pubdate_epoch});
+    }
+    foreach my $text (@texts) {
+        my $item = $feed->add_item($text->{full_uri});
+        $item->title(clean_html($text->{title}));
+        $item->guid(undef, isPermaLink => 1);
+        my @lines;
+        foreach my $method (qw/author title subtitle/) {
+            if (my $string = $text->{$method}) {
+                push @lines,
+                  '<strong>' . ucfirst($method) . '</strong>: ' . $string;
+            }
+        }
+        if (my $teaser = $text->{teaser}) {
+            push @lines, '<div>' . $teaser . '</div>';
+        }
+        $item->description('<div>' . join('<br>', @lines) . '</div>');
+        $item->pubDate($text->{pubdate_epoch});
+    }
+    $c->response->content_type('application/rss+xml');
+    $c->response->body($feed->to_string);
+}
+
 
 __PACKAGE__->meta->make_immutable;
 
