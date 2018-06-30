1f548192bd72c1e9ed68d988f09301852a734a46
Marco Pessotto <melmothx@gmail.com>
2018-06-30 11:50:31 +0200

    Add faster script in perl for mirroring

---
+++ script/mirror-site.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use FindBin;
+use File::Spec::Functions qw/catfile catdir/;
+use Cwd;
+chdir $FindBin::Bin or die;
+
+my $basedir = getcwd();
+my $wwwdir = catdir($basedir, "public_html");
+my $gitdir = catdir($basedir, "git");
+mkdir $wwwdir unless -d $wwwdir;
+mkdir $gitdir unless -d $gitdir;
+
+
+my ($spec, @wget_args) = @ARGV;
+
+die <<"HELP" unless $spec;
+Usage: $0 host:site_id [ optional wget arguments ]
+
+E.g. $0 amusewiki.org:amw --username User --password Password
+
+The host is the hostname without any protocol (HTTPS is always used).
+
+The site_id is the code used to fetch the git archive (optional).
+
+HELP
+
+
+mirror_site(split(/:/, $spec));
+
+
+sub mirror_site {
+    my ($sitename, $git) = @_;
+    chdir $basedir or die;
+    my $site = "https://$sitename";
+    chdir $wwwdir or die;
+    mkdir $sitename unless -d $sitename;
+    my $listing = catfile($sitename, 'mirror.ts.txt');
+    my $log = catfile($sitename, 'mirror.log');
+    unlink $log if -f $log;
+    system(wget => '-x', '-o', $log, "$site/mirror.ts.txt", @wget_args)
+      and warn "Failed to download $site/mirror.ts.txt, see $log for the details\n";
+    unless (-f $listing) {
+        warn "Couldn't retrieve $listing, skipping $sitename\n";
+        return;
+    }
+    my $urls = catfile($sitename, 'mirror.download');
+    open (my $out, '>', $urls) or die "$!";
+    open (my $fh, '<', $listing) or die "$!";
+    while (my $line = <$fh>) {
+        chomp $line;
+        my ($file, $ts) = split(/\#/, $line);
+        if ($file && $ts) {
+            my $target = catfile($sitename, mirror => $file);
+            if (-f $target) {
+                if ((stat($target))[9] != $ts) {
+                    my $fetch = "$site/mirror/$file\n";
+                    print $out $fetch;
+                    unlink $target;
+                }
+            }
+            else {
+                print $out "$site/mirror/$file\n";
+            }
+        }
+    }
+    close $fh;
+    close $out;
+    if (-f $urls and -s $urls) {
+        open (my $lst, '<', $urls) or die;
+        print <$lst>;
+        close $lst;
+        system(wget => '-a', $log, '-x', '-N', '-i', $urls, @wget_args)
+          and warn "Errors downloadning $urls, check $log";
+    }
+    chdir $gitdir or die;
+    if ($git) {
+        if (-d "$git") {
+            chdir $git;
+            system(git => pull => '--quiet') and warn "Couldn't pull $git";
+            chdir $basedir or die;
+        }
+        else {
+            system(git => clone => "git://$sitename/git/$git.git") and warn "Couldn't clone $git";
+        }
+    }
+                 
+    
+}
--- script/mirror-site.sh
+++ script/mirror-site.sh
@@ -1,53 +1,9 @@
 #!/bin/bash
 
 # Simple client to create a static mirror of an amusewiki instance.
-# E.g. 
-# set -x
-set -e
-site=$1
-if [ -z "$site" ]; then
-    echo "Usage: $0 https://my.site.org [ --user \"username\" --password \"password\" ]"
-    exit 2;
-fi
 
-shift
+echo "Replaced by mirror-site.pl in the same amusewiki directory"
 
-sitename=$(echo $site | sed -e 's,https\?://,,')
-
-log=$sitename/mirror.log
-mkdir -p $sitename
-rm -f $sitename/mirror.ts.txt
-wget -x -o $log "$@" $site/mirror.ts.txt || cat $log;
-
-
-if [ ! -f "$sitename/mirror.ts.txt" ]; then
-    echo "Couldn't download file list"
-    exit 2
-fi
-
-urls=$sitename/mirror.download
-rm -f $urls
-
-while read -r line ; do
-    file=$(echo $line | cut -f 1 -d '#');
-    ts=$(echo $line | cut -f 2 -d '#');
-    if [ -n "$file" -a -n "$ts" ]; then
-        if [ -f "$sitename/mirror/$file" ]; then
-            mine=$(stat -c "%Y" "$sitename/mirror/$file")
-            if [ $ts -ne $mine ]; then
-                echo "$site/mirror/$file" >> $urls
-                rm "$sitename/mirror/$file"
-            fi
-        else
-            echo "$site/mirror/$file" >> $urls
-        fi
-    fi
-done < $sitename/mirror.ts.txt
-
-if [ -f $urls ]; then
-    wget -a $log "$@" -x -N -i $urls
-fi
-
-
-
-
+cd $(dirname $0) || exit 2;
+basedir=$(pwd);
+$basedir/mirror-site.pl "$@"
