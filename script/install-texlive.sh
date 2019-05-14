bf0afb79e0f73fc9936736bd0452ac39fddb5aec
Marco Pessotto <melmothx@gmail.com>
2019-05-14 16:46:33 +0200

    Update install-texlive to 2019

--- script/install-texlive.sh
+++ script/install-texlive.sh
@@ -3,23 +3,23 @@ set -e
 
 TEXMIRROR=ctan.ijs.si/tex-archive
 cd "$HOME"
-echo "Installing TeX live 2018 in your home under texlive"
+echo "Installing TeX live 2019 in your home under texlive"
 # remove all stray files
 rm -rfv install-tl-*
 wget -O install-tl-unx.tar.gz \
      http://$TEXMIRROR/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzvf install-tl-unx.tar.gz
 # use shell expansion
-cd install-tl-201*
+cd install-tl-20*
 cat <<EOF >> amw.profile
 selected_scheme scheme-full
-TEXDIR $HOME/texlive/2018
-TEXMFCONFIG ~/.texlive2018/texmf-config
+TEXDIR $HOME/texlive/2019
+TEXMFCONFIG ~/.texlive2019/texmf-config
 TEXMFHOME ~/texmf
 TEXMFLOCAL $HOME/texlive/texmf-local
-TEXMFSYSCONFIG $HOME/texlive/2018/texmf-config
-TEXMFSYSVAR $HOME/texlive/2018/texmf-var
-TEXMFVAR ~/.texlive2018/texmf-var
+TEXMFSYSCONFIG $HOME/texlive/2019/texmf-config
+TEXMFSYSVAR $HOME/texlive/2019/texmf-var
+TEXMFVAR ~/.texlive2019/texmf-var
 option_doc 0
 option_src 0
 EOF
