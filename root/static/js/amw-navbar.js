8a3ae37e3a3d8f423c36414d6751cd268ac72abf
Marco Pessotto <melmothx@gmail.com>
2019-05-14 15:32:31 +0200

    Remove the sticky navbar and show it only when scrolling up
    Over the years I got really annoyed by the sticky navbar getting in
    the way of url #hash fragments, and related hacks which fail randomly.
    
    So try to put an end to this misery: show the sticky navbar only when
    scrolling up (back to reach the top) but not when jumping between
    anchors.
    
    This should do. There is no smooth transition, though, but it's not so
    important I think.

---
+++ root/static/js/amw-navbar.js
@@ -0,0 +1,19 @@
+/* the navbar is fixed only on scroll up */
+$(document).ready(function() {
+    var previous = 0;
+    var hash = window.location.hash || '';
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        var newhash = window.location.hash;
+        var hash_changed = hash != newhash ? 1 : 0;
+        console.log(scroll + ' ' + hash_changed);
+        if (scroll > 0 && scroll < previous && !hash_changed) {
+            $('#amw-nav-bar-top').addClass('navbar-fixed-top');
+        }
+        else {
+            $('#amw-nav-bar-top').removeClass('navbar-fixed-top');
+        }
+        previous = scroll;
+        hash = newhash;
+    })
+});
