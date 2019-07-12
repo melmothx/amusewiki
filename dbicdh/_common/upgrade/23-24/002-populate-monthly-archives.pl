b2c6d4633638139edf11d85c5e46165f503e4f98
Marco Pessotto <melmothx@gmail.com>
2019-07-12 12:54:16 +0200

    Disable old upgrade script

--- dbicdh/_common/upgrade/23-24/002-populate-monthly-archives.pl
+++ dbicdh/_common/upgrade/23-24/002-populate-monthly-archives.pl
@@ -1,5 +1,6 @@
 sub {
     my $schema = shift;
+    return;
     foreach my $site ($schema->resultset('Site')->all) {
         print "Populating monthly archives for " . $site->id . "\n";
         $site->populate_monthly_archives;
