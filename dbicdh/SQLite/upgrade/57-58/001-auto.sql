c4d8c8dc862e7d61d543915dc8645e85c77b4784
Marco Pessotto <melmothx@gmail.com>
2020-01-06 13:31:16 +0100

    Autogenerated DDLs

---
+++ dbicdh/SQLite/upgrade/57-58/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/57/001-auto.yml' to '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/58/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE title ADD COLUMN parent varchar(255);
+
+;
+
+COMMIT;
+
