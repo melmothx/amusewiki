22f98238ffe2c0895fdf114d12721cc32673ef4c
Marco Pessotto <melmothx@gmail.com>
2020-05-16 08:20:22 +0200

    Preliminary implementation for automatic toc labels
    This is an experimental feature. It is implemented on the front end
    side, given that its usage is web-specific (sharing links with a
    stable anchor) and appears to work.
    
    It needs to be made opt-in, at least at this stage. And cleaned up a
    bit. But I think it should work just fine.

--- root/static/js/amuse.js
+++ root/static/js/amuse.js
@@ -11,6 +11,39 @@ $(document).ready(function(){
     $(".center p").removeClass("text-justify").addClass("text-center");
     $(".right p").removeClass("text-justify").addClass("text-right");
     if ($("div.table-of-contents").length > 0) {
+
+        var toc_entries = Object.create(null);
+        $('.tableofcontentline').each(function() {
+            var el = $(this);
+            var text = el.text();
+            var anchor = el.find('a');
+            var old_id;
+            var new_id = text.replace(/[^\w\u00C0-\u02B8\u0386-\u052F]/g, '-')
+                .replace(/^-+/, '')
+                .replace(/--+/, '-');
+            var base_id = new_id;
+            var count = 1;
+            while (toc_entries[new_id]) {
+                new_id = base_id + '-' + count;
+                count++;
+            }
+            if (toc_entries[new_id]) {
+                console.log(new_id + ' is already taken');
+            }
+            else {
+                toc_entries[new_id] = 1;
+            }
+            if (anchor) {
+                old_id = anchor.attr('href');
+                if (old_id) {
+                    console.log(new_id + ' => ' + old_id);
+                    $(old_id).prepend($('<span>', { id: 'amw-toc-' + new_id }));
+                    anchor.attr('href', '#amw-toc-' + new_id);
+                }
+            }
+        });
+        console.log(toc_entries);
+
         $(".hidden-when-no-toc").show();
         var clonedtoc = $("div.table-of-contents").clone();
         clonedtoc.show();
