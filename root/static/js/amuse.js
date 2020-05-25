dcf8d49f3e1766a773a3fe93ee701195cf09824a
Marco Pessotto <melmothx@gmail.com>
2020-05-25 11:11:21 +0200

    Use the named toc in previews

--- root/static/js/amuse.js
+++ root/static/js/amuse.js
@@ -1,3 +1,36 @@
+function use_named_toc () {
+    var toc_entries = Object.create(null);
+    $('.tableofcontentline').each(function() {
+        var el = $(this);
+        var text = el.text();
+        var anchor = el.find('a');
+        var old_id;
+        var new_id = text.replace(/[^\w\u00C0-\u02B8\u0386-\u052F]/g, '-')
+            .replace(/^-+/, '')
+            .replace(/--+/, '-');
+        var base_id = new_id;
+        var count = 1;
+        while (toc_entries[new_id]) {
+            new_id = base_id + '-' + count;
+            count++;
+        }
+        if (toc_entries[new_id]) {
+            console.log(new_id + ' is already taken');
+        }
+        else {
+            toc_entries[new_id] = 1;
+        }
+        if (anchor) {
+            old_id = anchor.attr('href');
+            if (old_id) {
+                console.log(new_id + ' => ' + old_id);
+                $(old_id).prepend($('<span>', { id: 'amw-toc-' + new_id }));
+                anchor.attr('href', '#amw-toc-' + new_id);
+            }
+        }
+    });
+    console.log(toc_entries);
+}
 
 $(document).ready(function(){
     $("p").each(function(i) {
@@ -14,37 +47,7 @@ $(document).ready(function(){
     var has_cover = $("#text-cover-img").length;
     if (has_toc > 0) {
         var toc_entries = Object.create(null);
-        $('.tableofcontentline').each(function() {
-            var el = $(this);
-            var text = el.text();
-            var anchor = el.find('a');
-            var old_id;
-            var new_id = text.replace(/[^\w\u00C0-\u02B8\u0386-\u052F]/g, '-')
-                .replace(/^-+/, '')
-                .replace(/--+/, '-');
-            var base_id = new_id;
-            var count = 1;
-            while (toc_entries[new_id]) {
-                new_id = base_id + '-' + count;
-                count++;
-            }
-            if (toc_entries[new_id]) {
-                console.log(new_id + ' is already taken');
-            }
-            else {
-                toc_entries[new_id] = 1;
-            }
-            if (anchor) {
-                old_id = anchor.attr('href');
-                if (old_id) {
-                    console.log(new_id + ' => ' + old_id);
-                    $(old_id).prepend($('<span>', { id: 'amw-toc-' + new_id }));
-                    anchor.attr('href', '#amw-toc-' + new_id);
-                }
-            }
-        });
-        console.log(toc_entries);
-
+        use_named_toc();
         $('.tableofcontentline').each(function() {
             var el = $(this);
             var anchor = el.find('a');
