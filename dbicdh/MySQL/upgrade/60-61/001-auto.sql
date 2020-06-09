a489f8170377aec212a4ca4836412765eb2c01ed
Marco Pessotto <melmothx@gmail.com>
2020-06-09 07:30:57 +0200

    Add autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/60-61/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/60/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/61/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `whitelist_ip` ADD COLUMN `user_editable` smallint NOT NULL DEFAULT 0;
+
+;
+
+COMMIT;
+
