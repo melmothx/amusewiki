f14df9c1764c4a88636aac072954c9869c777812
Marco Pessotto <melmothx@gmail.com>
2021-01-09 08:48:03 +0100

    Add autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/63-64/001-auto.sql
@@ -0,0 +1,16 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/63/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/64/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `title` ADD COLUMN `publisher` text NULL,
+                    ADD COLUMN `isbn` text NULL,
+                    ADD COLUMN `rights` text NULL,
+                    ADD COLUMN `seriesname` text NULL,
+                    ADD COLUMN `seriesnumber` text NULL;
+
+;
+
+COMMIT;
+
---
+++ dbicdh/SQLite/upgrade/63-64/001-auto.sql
@@ -0,0 +1,24 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/63/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/64/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE title ADD COLUMN publisher text;
+
+;
+ALTER TABLE title ADD COLUMN isbn text;
+
+;
+ALTER TABLE title ADD COLUMN rights text;
+
+;
+ALTER TABLE title ADD COLUMN seriesname text;
+
+;
+ALTER TABLE title ADD COLUMN seriesnumber text;
+
+;
+
+COMMIT;
+
