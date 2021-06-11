57ef5bae9a6d780ec0160ce80544d18e94a2edd1
Marco Pessotto <melmothx@gmail.com>
2021-06-11 16:17:25 +0200

    Add autogenerated DDLs

---
+++ dbicdh/PostgreSQL/upgrade/65-66/001-auto.sql
@@ -0,0 +1,15 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/65/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/66/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE "custom_formats" ADD COLUMN "bb_geometry_top_margin" integer DEFAULT 0 NOT NULL;
+
+;
+ALTER TABLE "custom_formats" ADD COLUMN "bb_geometry_outer_margin" integer DEFAULT 0 NOT NULL;
+
+;
+
+COMMIT;
+
