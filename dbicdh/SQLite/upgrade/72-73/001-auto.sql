87be27f3f3ead132e8979ada95f1ef6c7332c094
Marco Pessotto <melmothx@gmail.com>
2022-06-05 09:46:46 +0200

    Autogenerated DDLs

---
+++ dbicdh/SQLite/upgrade/72-73/001-auto.sql
@@ -0,0 +1,15 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/72/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/73/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE muse_header ADD COLUMN muse_value_html text;
+
+;
+ALTER TABLE site_category_type ADD COLUMN generate_index smallint NOT NULL DEFAULT 1;
+
+;
+
+COMMIT;
+
