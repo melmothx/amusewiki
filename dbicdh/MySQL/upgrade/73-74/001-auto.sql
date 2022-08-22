f966aba5590e3170f8e2998a6d745cf2f9c301c9
Marco Pessotto <melmothx@gmail.com>
2022-08-22 10:32:17 +0200

    Autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/73-74/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/73/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/74/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `site_category_type` ADD COLUMN `in_colophon` smallint NOT NULL DEFAULT 0;
+
+;
+
+COMMIT;
+
