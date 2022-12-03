98562b7a952d86dc90a19e4d8f90d951b41664e2
Marco Pessotto <melmothx@gmail.com>
2022-12-03 08:42:53 +0100

    Add autogenerated DDLs

---
+++ dbicdh/SQLite/upgrade/76-77/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/76/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/77/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE attachment ADD COLUMN alt_text text;
+
+;
+
+COMMIT;
+
