ab5700698983cc6300a879eb6142c8675b4d54f7
Marco Pessotto <melmothx@gmail.com>
2023-05-03 17:43:55 +0200

    Try to correct the navbar toggling
    See:
    https://stackoverflow.com/questions/58867894/how-to-stop-content-from-jumping-after-navbar-becomes-fixed-on-scroll
    
    Although it does not seem to fix much. Unclear why it should be twice
    the height.

--- root/static/js/amw-navbar.js
+++ root/static/js/amw-navbar.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     var previous = 0;
     var hash = window.location.hash || '';
     $(window).scroll(function() {
+        var navbarheight = $('#amw-nav-bar-top').height();
         var scroll = $(window).scrollTop();
         var newhash = window.location.hash || '';
         if (hash === newhash && scroll > 0 && scroll < previous) {
             if ((previous - scroll) > 120) {
                 $('#amw-nav-bar-top').addClass('navbar-fixed-top');
+                $('body').css('margin-top', navbarheight * 2);
                 previous = scroll;
             }
             /*
@@ -18,6 +20,7 @@ $(document).ready(function() {
         }
         else {
             $('#amw-nav-bar-top').removeClass('navbar-fixed-top');
+            $('body').css('margin-top', 0);
             previous = scroll;
         }
         hash = newhash;
