2feb4e88775660c11bdda1c88a702b06370843ee
Marco Pessotto <melmothx@gmail.com>
2023-05-12 11:32:59 +0200

    Autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/78-79/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/78/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/79/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `oai_pmh_record` ADD COLUMN `update_run` integer NOT NULL DEFAULT 0;
+
+;
+
+COMMIT;
+
