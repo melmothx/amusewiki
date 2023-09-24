928a6ab17704005ac2d066de5b8df393d8de5c99
Marco Pessotto <melmothx@gmail.com>
2023-09-24 07:47:14 +0200

    Add autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/79-80/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/79/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/80/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `attachment` ADD COLUMN `errors` text NULL;
+
+;
+
+COMMIT;
+
