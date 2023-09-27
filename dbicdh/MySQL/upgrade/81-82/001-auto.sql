6d575b104e983fe3761a5193b77b0c8c0c21c1b6
Marco Pessotto <melmothx@gmail.com>
2023-09-27 15:42:39 +0200

    Autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/81-82/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/81/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/82/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `title` ADD COLUMN `datefirst` text NULL;
+
+;
+
+COMMIT;
+
