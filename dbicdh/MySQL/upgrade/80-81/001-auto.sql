d828e405ef88078652e525ddbaadb06775f4f094
Marco Pessotto <melmothx@gmail.com>
2023-09-27 10:56:33 +0200

    Autogenerated DDLs

---
+++ dbicdh/MySQL/upgrade/80-81/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/80/001-auto.yml' to '/home/marco/amw/AmuseWikiFarm/dbicdh/_source/deploy/81/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE `oai_pmh_record` ADD COLUMN `metadata_format_description` varchar(255) NULL;
+
+;
+
+COMMIT;
+
