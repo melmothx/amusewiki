986ddd4367f17144dbfa872f5d3988e3b11e7a81
Marco Pessotto <melmothx@gmail.com>
2024-01-22 17:14:34 +0100

    Test node-related workflow
    Prevent obviously recursive node

--- lib/AmuseWikiFarm/Schema/ResultSet/Node.pm
+++ lib/AmuseWikiFarm/Schema/ResultSet/Node.pm
@@ -6,6 +6,7 @@ use warnings;
 use base 'DBIx::Class::ResultSet';
 use HTML::Entities qw/encode_entities/;
 use AmuseWikiFarm::Log::Contextual;
+use AmuseWikiFarm::Utils::Amuse;
 
 sub hri {
     return shift->search(undef, { result_class => 'DBIx::Class::ResultClass::HashRefInflator' });
@@ -27,18 +28,14 @@ sub root_nodes {
 sub update_or_create_from_params {
     my ($self, $params, $opts) = @_;
     $opts ||= {};
-    if (my $uri = $params->{uri}) {
-        if ($uri =~ m/([a-z0-9][a-z0-9-]*[a-z0-9])/) {
-            $uri = $1;
-            $uri =~ s/--+/-/g;
-            if ($opts->{create}) {
-                return if $self->find({ uri => $uri });
-            }
-            my $node = $self->find_or_create({ uri => $uri });
-            $node->discard_changes;
-            $node->update_from_params($params);
-            return $node;
+    if (my $uri = AmuseWikiFarm::Utils::Amuse::muse_naming_algo($params->{uri})) {
+        if ($opts->{create}) {
+            return if $self->find({ uri => $uri });
         }
+        my $node = $self->find_or_create({ uri => $uri });
+        $node->discard_changes;
+        $node->update_from_params($params);
+        return $node;
     }
 }
 
