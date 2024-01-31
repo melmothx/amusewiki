30e72380798015efbb38ae96e02e0e1a4fa27959
Marco Pessotto <melmothx@gmail.com>
2024-01-31 16:56:33 +0100

    Autogenerated DDLs

---
+++ dbicdh/MySQL/deploy/85/001-auto-__VERSION.sql
@@ -0,0 +1,18 @@
+--
+-- Created by SQL::Translator::Producer::MySQL
+-- Created on Wed Jan 31 14:35:04 2024
+--
+;
+SET foreign_key_checks=0;
+--
+-- Table: `dbix_class_deploymenthandler_versions`
+--
+CREATE TABLE `dbix_class_deploymenthandler_versions` (
+  `id` integer NOT NULL auto_increment,
+  `version` varchar(50) NOT NULL,
+  `ddl` text NULL,
+  `upgrade_sql` text NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `dbix_class_deploymenthandler_versions_version` (`version`)
+);
+SET foreign_key_checks=1;
